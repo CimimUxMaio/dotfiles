5843ae5d265deb6d6078519dbfb5cad06b8dbe4c
CimimUxMaio <juanbiabdon@gmail.com>
2023-01-28 17:45:22 -0300

    Make format on save true by default

--- config/nvim/lua/plugins/null-ls.lua
+++ config/nvim/lua/plugins/null-ls.lua
@@ -5,33 +5,41 @@ local diagnostics = null_ls.builtins.diagnostics
 local code_actions = null_ls.builtins.code_actions
 
 null_ls.setup({
-    debug = false,
-    sources = {
-        formatting.black,
-        formatting.eslint,
+  debug = false,
+  sources = {
+    formatting.autopep8,
+    formatting.eslint,
 
-        diagnostics.flake8,
-        diagnostics.eslint,
-        diagnostics.misspell,
+    diagnostics.flake8,
+    diagnostics.eslint,
+    diagnostics.misspell,
 
-        code_actions.eslint,
-    }
+    code_actions.eslint,
+  }
 })
 
-_G.format_on_save = false  -- By default disable auto formatting
+
+
+local function set_format_on_save_augroup()
+  vim.cmd [[
+    augroup FormatOnSave
+      au!
+      autocmd BufWritePre * lua vim.lsp.buf.format()
+    augroup END
+  ]]
+end
+
+set_format_on_save_augroup()
+_G.format_on_save = true -- Set format on save to true by default.
+
 function _G.toggle_format_on_save()
-    _G.format_on_save = not _G.format_on_save
-    if (not _G.format_on_save) then
-        vim.cmd("autocmd! FormatOnSave")
-    else
-        vim.cmd [[
-            augroup FormatOnSave
-                au!
-                autocmd BufWritePre * lua vim.lsp.buf.formatting_sync()
-            augroup END
-        ]]
-    end
-    vim.notify("Format on save set to: " .. tostring(_G.format_on_save), "info", { title = "Format on Save" })
+  _G.format_on_save = not _G.format_on_save
+  if (not _G.format_on_save) then
+    vim.cmd("autocmd! FormatOnSave")
+  else
+    set_format_on_save_augroup()
+  end
+  vim.notify("Format on save set to: " .. tostring(_G.format_on_save), "info", { title = "Format on Save" })
 end
 
 vim.cmd("command -nargs=0 ToggleFormatOnSave lua toggle_format_on_save()")
