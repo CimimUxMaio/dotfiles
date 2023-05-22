3262cddc10b25d350db47cc347989695285341f3
CimimUxMaio <juanbiabdon@gmail.com>
2023-05-22 20:36:39 -0300

    Ability to fold code using treesitter

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -29,3 +29,10 @@ configs.setup {
     }
   }
 }
+
+-- Enable code folding using treesitter
+local opt = vim.opt
+
+opt.foldlevel = 20
+opt.foldmethod = "expr"
+opt.foldexpr = "nvim_treesitter#foldexpr()"
