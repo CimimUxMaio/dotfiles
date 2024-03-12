2cebd6125466f09f79a0307769454b5e7239b3d3
CimimUxMaio <juanbiabdon@gmail.com>
2024-03-12 18:57:42 -0300

    Telescope key bindings

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,25 +1,27 @@
 local _M = {}
 
 function _M.setup()
-  local actions = require("telescope.actions")
+	local actions = require("telescope.actions")
 
-  require("telescope").setup({
-    defaults = {
-      mappings = {
-        i = {
-          ["<C-j>"] = actions.move_selection_next,
-          ["<C-k>"] = actions.move_selection_previous,
-          ["<C-c>"] = actions.close,
-          ["<C-s>"] = actions.select_vertical,
-          ["<C-h><C-s>"] = actions.select_horizontal,
-        }
-      }
-    }
-  })
+	require("telescope").setup({
+		defaults = {
+			mappings = {
+				i = {
+					["<C-j>"] = actions.move_selection_next,
+					["<C-k>"] = actions.move_selection_previous,
+					["<C-c>"] = actions.close,
+					["<C-s>"] = actions.select_vertical,
+					["<C-h><C-s>"] = actions.select_horizontal,
+				},
+			},
+		},
+	})
 
-
-  vim.api.nvim_set_keymap("n", "<leader><leader>", "<cmd>Telescope find_files<CR>", { noremap = true, silent = true })
-  vim.api.nvim_set_keymap("n", "<leader>P", "<cmd>Telescope commands<CR>", { noremap = true, silent = true })
+	vim.api.nvim_set_keymap("n", "<leader><leader>", "<cmd>Telescope find_files<CR>", { noremap = true, silent = true })
+	vim.api.nvim_set_keymap("n", "<leader>?", "<cmd>Telescope help_tags<CR>", { noremap = true, silent = true })
+	vim.api.nvim_set_keymap("n", "<leader>P", "<cmd>Telescope commands<CR>", { noremap = true, silent = true })
+	vim.api.nvim_set_keymap("n", "<leader>D", "<cmd>Telescope diagnostics<CR>", { noremap = true, silent = true })
+	vim.api.nvim_set_keymap("n", "<leader>S", "<cmd>Telescope live_grep<CR>", { noremap = true, silent = true })
 end
 
 return _M
