cd6860aa2f852836c5fdea33bb3ef96333bad2db
CimimUxMaio <juanbiabdon@gmail.com>
2024-03-13 19:02:54 -0300

    Window split key bindings

--- .config/nvim/lua/general/mappings.lua
+++ .config/nvim/lua/general/mappings.lua
@@ -1,16 +1,12 @@
-
 local opts = { noremap = true, silent = true }
-
 vim.api.nvim_set_keymap("n", "<C-c>", "<Esc>", opts)
 vim.api.nvim_set_keymap("i", "<C-c>", "<Esc>", opts)
-
 
 -- Moving between windows
 vim.api.nvim_set_keymap("n", "<C-j>", "<C-W><C-j>", opts)
 vim.api.nvim_set_keymap("n", "<C-k>", "<C-W><C-k>", opts)
 vim.api.nvim_set_keymap("n", "<C-l>", "<C-W><C-l>", opts)
 vim.api.nvim_set_keymap("n", "<C-h>", "<C-W><C-h>", opts)
-
 
 -- Rezising windows
 -- M: Alt
@@ -19,6 +15,9 @@ vim.api.nvim_set_keymap("n", "<M-k>", ":resize +2<CR>", opts)
 vim.api.nvim_set_keymap("n", "<M-h>", ":vertical resize -2<CR>", opts)
 vim.api.nvim_set_keymap("n", "<M-l>", ":vertical resize +2<CR>", opts)
 
+-- Window Splits
+vim.api.nvim_set_keymap("n", "<C-s>", ":split<CR>", opts)
+vim.api.nvim_set_keymap("n", "<C-v><C-s>", ":vsplit<CR>", opts)
 
 -- Better tabing
 vim.api.nvim_set_keymap("v", "<", "<gv", opts)
