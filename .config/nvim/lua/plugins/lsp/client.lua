f0d40af1167742b7d357db8bb5dc96ae964e9e16
CimimUxMaio <juanbiabdon@gmail.com>
2024-03-14 20:02:06 -0300

    Lsp keybindings + lsp rename with fallback to treesitter smart rename.

--- .config/nvim/lua/plugins/lsp/client.lua
+++ .config/nvim/lua/plugins/lsp/client.lua
@@ -1,4 +1,4 @@
-local function lsp_highlight_document(bufnr)
+local function set_highlight_on_hover(bufnr)
 	vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
 		buffer = bufnr,
 		callback = vim.lsp.buf.document_highlight,
@@ -10,40 +10,38 @@ local function lsp_highlight_document(bufnr)
 	})
 end
 
-local function lsp_keymaps(bufnr)
+local function set_lsp_keymaps(client, bufnr)
 	local opts = { noremap = true, silent = true }
 
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-	-- vim.api.nvim_buf_set_keymap(bufnr, "n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-	-- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-	-- vim.api.nvim_buf_set_keymap(bufnr, "n", "[d", '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>', opts)
 
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "F", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
 
-	-- vim.api.nvim_buf_set_keymap(bufnr, "n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
+	if client.server_capabilities.renameProvider then
+		vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>r", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+	end
+
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>e", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
-	-- vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
+
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "dk", '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "dj", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
 end
 
-local function lsp_attach(client, bufnr)
-	-- client.server_capabilities.document_formatting = false
-
-	lsp_keymaps(bufnr)
+local function on_lsp_attach(client, bufnr)
+	set_lsp_keymaps(client, bufnr)
 
 	if client.supports_method("textDocument/documentHighlight") then
-		lsp_highlight_document(bufnr)
+		set_highlight_on_hover(bufnr)
 	end
 end
 
 require("mason-lspconfig").setup_handlers({
 	function(server_name)
 		require("lspconfig")[server_name].setup({
-			on_attach = lsp_attach,
+			on_attach = on_lsp_attach,
 			capabilities = require("cmp_nvim_lsp").default_capabilities(),
 		})
 	end,
