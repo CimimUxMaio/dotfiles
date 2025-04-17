a624a4d27fa4dd8a313863e74bd03bb3594b53cd
CimimUxMaio <juanbiabdon@gmail.com>
2025-04-17 14:16:38 -0300

    Make linter not raise errors when missing
    `nvim-lint` now does not raise an error whenever a linter
    is not installed.

--- .config/nvim/lua/plugins/nvim-lint.lua
+++ .config/nvim/lua/plugins/nvim-lint.lua
@@ -3,7 +3,7 @@ return {
 
   name = "nvim-lint",
 
-  config = function()
+  init = function()
     require("lint").linters_by_ft = {
       javascript = { "eslint_d" },
       javascriptreact = { "eslint_d" },
@@ -19,7 +19,7 @@ return {
 
     vim.api.nvim_create_autocmd({ "BufWritePost" }, {
       callback = function()
-        require("lint").try_lint()
+        require("lint").try_lint(nil, { ignore_errors = true })
       end,
     })
   end,
